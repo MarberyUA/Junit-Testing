75781f3c7748d095a116d5eb3c18e0649d0a1b70
Rostislav Levadnyi <rlevadniy@ukr.net>
2020-06-02 13:12:08 +0300

    Test methods naming were refactored & now methods should throw an exceptions

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,57 +1,26 @@
 public class Calculator {
-    private Double firstValue;
-    private String mathematicalOperation;
-    private Double secondValue ;
 
-    public Calculator() {
-
-    }
-
-    public Calculator(Number firstValue, String mathematicalOperation, Number secondValue) {
-        this.firstValue = firstValue.doubleValue();
-        this.mathematicalOperation = mathematicalOperation.replaceAll(" ", "");
-        this.secondValue = secondValue.doubleValue();
-    }
-
-    public Number getFirstValue() {
-        return firstValue;
-    }
-
-    public void setFirstValue(Number firstValue) {
-        this.firstValue = firstValue.doubleValue();
-    }
-
-    public String getMathematicalOperation() {
-        return mathematicalOperation;
-    }
-
-    public void setMathematicalOperation(String mathematicalOperation) {
-        this.mathematicalOperation = mathematicalOperation.replaceAll(" ", "");
-    }
-
-    public Number getSecondValue() {
-        return secondValue;
-    }
-
-    public void setSecondValue(Number secondValue) {
-        this.secondValue = secondValue.doubleValue();
-    }
-
-    public Double calculate() {
-        if (firstValue == null || (secondValue == null || secondValue == 0)) {
-            return null;
+    public Double calculate(Number firstValue, String mathematicalOperation, Number secondValue) {
+        if (firstValue == null || secondValue == null) {
+            throw new IllegalArgumentException("");
+        }
+        Double doubleFirstValue = firstValue.doubleValue();
+        Double doubleSecondValue = secondValue.doubleValue();
+        mathematicalOperation = mathematicalOperation.replaceAll(" ", "");
+        if (mathematicalOperation.equals("/") && doubleSecondValue == 0) {
+            throw new IllegalArgumentException("");
         }
         switch (mathematicalOperation) {
             case ("*"):
-                return firstValue * secondValue;
+                return doubleFirstValue * doubleSecondValue;
             case ("+"):
-                return firstValue + secondValue;
+                return doubleFirstValue + doubleSecondValue;
             case ("-"):
-                return firstValue - secondValue;
+                return doubleFirstValue - doubleSecondValue;
             case ("/"):
-                return firstValue / secondValue;
+                return doubleFirstValue / doubleSecondValue;
             default:
-                return null;
+                throw new ArithmeticException("");
         }
     }
 }
